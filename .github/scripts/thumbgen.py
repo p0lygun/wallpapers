98b3395a9779d5f440112d82d007a0b7f779c293
Vibhakar Solanki <solankivibhakar82@gmail.com>
2021-10-18 13:36:23 +0000

    Create thumbgen.py

---
+++ .github/scripts/thumbgen.py
@@ -0,0 +1,10 @@
+import pathlib
+with open('README_BASE.md', 'r') as FILE:
+    readme = FILE.read()
+
+wallpapers = pathlib.Path("rice/wallpapers/").glob("*")
+for wallpaper in wallpapers:
+    readme += f'<img src="{wallpaper.as_posix()}" width="250">'
+
+with open('README.md', 'w') as FILE:
+    FILE.write(readme)
