bd51d3e37bb4795325f5db5a742e8576ed48cecb
p0lygun <solankivibhakar82@gmail.com>
2021-10-19 15:25:58 +0530

    use relative path in README.md

--- .github/scripts/thumbgen.py
+++ .github/scripts/thumbgen.py
@@ -15,7 +15,7 @@ for wallpaper in wallpapers:
     # gives # Mon, 18 Oct 2021 18:53:41 +0000
     git_out = subprocess.run(git_cmd, stdout=subprocess.PIPE).stdout.decode('utf-8').strip()
     t = datetime.strptime(git_out, "%a, %d %b %Y %H:%M:%S %z")
-    time_mapping.append([wallpaper, t])
+    time_mapping.append([wallpaper.relative_to(root), t])
 time_mapping.sort(key=lambda x: x[1], reverse=True)
 
 # print(time_mapping)
