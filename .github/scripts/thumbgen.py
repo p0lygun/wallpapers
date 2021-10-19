8974cb10d543c9d62b184dc2a44a0e402565d1d1
p0lygun <solankivibhakar82@gmail.com>
2021-10-19 15:20:15 +0530

    now sorts file in order they where added to repo

--- .github/scripts/thumbgen.py
+++ .github/scripts/thumbgen.py
@@ -1,10 +1,26 @@
 import pathlib
-with open('.github/README_BASE.md', 'r') as FILE:
+import subprocess
+import os
+from datetime import datetime
+root = pathlib.Path(__file__).parents[2]
+
+with open(root / ".github" / "README_BASE.md", 'r') as FILE:
     readme = FILE.read()
 
-wallpapers = pathlib.Path("wallpapers/").glob("*")
-for wallpaper in wallpapers:
-    readme += f'<img src="{wallpaper.as_posix()}" width="250">'
+wallpapers = (root / "wallpapers").glob("*")
+time_mapping = []
 
-with open('README.md', 'w') as FILE:
+for wallpaper in wallpapers:
+    git_cmd = ["git", "log", "--diff-filter=A", "--follow", "--format=%aD", "-1", "--", f"wallpapers/{wallpaper.name}"]
+    # gives # Mon, 18 Oct 2021 18:53:41 +0000
+    git_out = subprocess.run(git_cmd, stdout=subprocess.PIPE).stdout.decode('utf-8').strip()
+    t = datetime.strptime(git_out, "%a, %d %b %Y %H:%M:%S %z")
+    time_mapping.append([wallpaper, t])
+time_mapping.sort(key=lambda x: x[1], reverse=True)
+
+# print(time_mapping)
+for wallpaper in time_mapping:
+    readme += f'<img src="{wallpaper[0].as_posix()}" width="250">'
+
+with open(root / "README.md", 'w') as FILE:
     FILE.write(readme)
